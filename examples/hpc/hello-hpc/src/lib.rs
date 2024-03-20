f1825c60c656d235da6ffaff74e22993c232909d
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2024-03-20 16:09:00 +0200

    Add macro to print test name

--- examples/hpc/hello-hpc/src/lib.rs
+++ examples/hpc/hello-hpc/src/lib.rs
@@ -10,3 +10,14 @@ pub fn uart_write(s: &str) {
         unsafe { ptr::write_volatile(UART0_ADDR as *mut u8, *b) };
     }
 }
+
+/// Print the name of the current file, i.e., test name.
+///
+/// This must be a macro to make sure core::file matches the file this is
+/// invoked in.
+#[macro_export]
+macro_rules! print_example_name {
+    () => {
+        sprintln!("[{}]", core::file!());
+    };
+}
