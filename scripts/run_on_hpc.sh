c5b8f41a92ff5e5236e809912bb3cd9a629125d3
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2024-03-22 14:31:24 +0200

    Check for param existence

--- scripts/run_on_hpc.sh
+++ scripts/run_on_hpc.sh
@@ -6,4 +6,9 @@ BASEDIR=$(dirname "$0")
 $BASEDIR/setup_renode.sh
 
 BIN=${BIN=$1}
+if [ -z "$BIN" ]; then
+    echo "!! Pass in an ELF using \$BIN or \$1"
+    exit 1
+fi
+
 renode --console -e "set bin @$BIN; include @$BASEDIR/resc/2_run_hpc.resc"
