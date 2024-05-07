bf18fa5d3ae7c5a57637c6ced123095a7cff9780
Andreas Stergiopoulos <andreas.stergiopoulos@tuni.fi>
2024-05-07 14:30:09 +0300

    Moved timer example to headsail-bsp examples

--- examples/headsail-bsp/examples/timer0.rs
+++ examples/headsail-bsp/examples/timer0.rs
@@ -3,12 +3,8 @@
 
 use headsail_bsp::{rt::entry, sprintln, timer_unit::*};
 
-use hello_hpc::print_example_name;
-use panic_halt as _;
-
 #[entry]
 fn main() -> ! {
-    print_example_name!();
     sprintln!("Timer0 example");
     let cnt_start = timer0_get_count();
     sprintln!("Timer0 counter value at start: {}", cnt_start);
