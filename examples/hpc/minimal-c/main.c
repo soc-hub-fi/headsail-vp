c36c0743ddc0db867a5a535903c10919ebdfa9ce
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2024-05-15 20:04:14 +0300

    C: add native print

--- examples/hpc/minimal-c/main.c
+++ examples/hpc/minimal-c/main.c
@@ -1,5 +1,21 @@
+#include <stdint.h>
+#include <string.h>
+
 #include "boot.h"
 
+const uintptr_t UART0_ADDR = 0xFFF00000;
+uint8_t *const UART0_PTR = (uint8_t*)UART0_ADDR;
+
+void putc(char ch) {
+    *UART0_PTR |= ch;
+}
+
 int main() {
+    const char *str = "Hello world";
+
+    for (int i = 0; i < strlen(str); i++) {
+        putc(str[i]);
+    }
+
     return 0;
 }
