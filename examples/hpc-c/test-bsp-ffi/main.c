37a797b4ddad8b42b69a3a82f36ee074583328e0
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2024-05-16 14:10:03 +0300

    Make test-bsp-ffi into hello

--- examples/hpc-c/test-bsp-ffi/main.c
+++ examples/hpc-c/test-bsp-ffi/main.c
@@ -4,11 +4,18 @@
 #include "boot.h"
 #include "headsail_bsp.h"
 
-int main() {
-    const char *str = "Hello world (bsp::putc)";
+int main()
+{
+    const char *hello = "Hello world!";
+    const char *str2 = "This is the BSP printing.";
 
-    for (int i = 0; i < strlen(str); i++) {
-        putc(str[i]);
+    for (int i = 0; i < strlen(hello); i++)
+    {
+        putc(hello[i]);
+    }
+    for (int i = 0; i < strlen(str2); i++)
+    {
+        putc(hello[i]);
     }
 
     return 0;
