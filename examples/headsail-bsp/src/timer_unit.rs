357b624f4d42790cfe8d38c36624297514d0a349
Andreas Stergiopoulos <andreas.stergiopoulos@tuni.fi>
2024-05-17 11:26:39 +0300

    Relocatable timer for timer_unit

--- examples/headsail-bsp/src/timer_unit.rs
+++ examples/headsail-bsp/src/timer_unit.rs
@@ -11,46 +11,52 @@ use crate::{mmap::TIMER0_ADDR, read_u32, write_u32};
 
 use bit_field::BitField;
 
-const TIMER0_CTRL_REG_OFFSET: usize = 0x0;
-const TIMER0_COUNTER_REG_OFFSET: usize = 0x8;
-const TIMER0_ENABLE_BIT: usize = 0b0;
+pub struct TimerUnit<const BASE_ADDRESS: usize>;
 
-/**
- * Enables the timer (starts counting).
- */
-#[inline]
-pub fn timer0_enable() {
-    // Read register
-    let mut reg = read_u32(TIMER0_ADDR + TIMER0_CTRL_REG_OFFSET);
-    // Make enable bit 1
-    reg.set_bit(TIMER0_ENABLE_BIT, true);
-    // Write register back
-    write_u32(TIMER0_ADDR + TIMER0_CTRL_REG_OFFSET, reg);
+impl<const BASE_ADDRESS: usize> TimerUnit<BASE_ADDRESS>{
+    const TIMER_CTRL_REG_OFFSET: usize = 0x0;
+    const TIMER_COUNTER_REG_OFFSET: usize = 0x8;
+    const TIMER_ENABLE_BIT: usize = 0b0;
+
+    /**
+     * Enables the timer (starts counting).
+     */
+    #[inline]
+    pub fn enable() {
+        // Read register
+        let mut reg = read_u32(BASE_ADDRESS + Self::TIMER_CTRL_REG_OFFSET);
+        // Make enable bit 1
+        reg.set_bit(Self::TIMER_ENABLE_BIT, true);
+        // Write register back
+        write_u32(BASE_ADDRESS + Self::TIMER_CTRL_REG_OFFSET, reg);
+    }
+
+    /**
+     * Disables the timer (stops counting).
+     */
+    #[inline]
+    pub fn disable() {
+        // Read register
+        let mut reg = read_u32(BASE_ADDRESS + Self::TIMER_CTRL_REG_OFFSET);
+        // Write 0 to bit 0 but leave all other bits untouched
+        reg.set_bit(Self::TIMER_ENABLE_BIT as usize, false);
+        // Write register back
+        write_u32(BASE_ADDRESS + Self::TIMER_CTRL_REG_OFFSET, reg);
+    }
+
+    /**
+     * Returns the timer counter (tick value).
+     */
+    #[inline]
+    pub fn get_count() -> u32 {
+        return read_u32(BASE_ADDRESS + Self::TIMER_COUNTER_REG_OFFSET);
+    }
+
+    #[inline]
+    #[cfg(debug_assertions)]
+    pub fn get_ctrl_reg() -> u32 {
+        return read_u32(BASE_ADDRESS + Self::TIMER_CTRL_REG_OFFSET);
+    }
 }
 
-/**
- * Disables the timer (stops counting).
- */
-#[inline]
-pub fn timer0_disable() {
-    // Read register
-    let mut reg = read_u32(TIMER0_ADDR + TIMER0_CTRL_REG_OFFSET);
-    // Write 0 to bit 0 but leave all other bits untouched
-    reg.set_bit(TIMER0_ENABLE_BIT as usize, false);
-    // Write register back
-    write_u32(TIMER0_ADDR + TIMER0_CTRL_REG_OFFSET, reg);
-}
-
-/**
- * Returns the timer counter (tick value).
- */
-#[inline]
-pub fn timer0_get_count() -> u32 {
-    return read_u32(TIMER0_ADDR + TIMER0_COUNTER_REG_OFFSET);
-}
-
-#[inline]
-#[cfg(debug_assertions)]
-pub fn timer0_get_ctrl_reg() -> u32 {
-    return read_u32(TIMER0_ADDR + TIMER0_CTRL_REG_OFFSET);
-}
+pub type Timer0 = TimerUnit::<TIMER0_ADDR>;
