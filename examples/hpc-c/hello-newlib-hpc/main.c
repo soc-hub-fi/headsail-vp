fd32828a75a59e500fe0a765a384343d6684acbe
Andreas Stergiopoulos <andreas.stergiopoulos@tuni.fi>
2024-07-09 10:04:32 +0300

    Adds clarifying comments to main.c

--- examples/hpc-c/hello-newlib-hpc/main.c
+++ examples/hpc-c/hello-newlib-hpc/main.c
@@ -9,6 +9,7 @@ int main()
 {
     printf("Hello, world! (from newlib)\n");   
     
+    /** Generate two double arrays, one for data and one for results */
     double num[10];
     double res[10];
 
@@ -17,6 +18,10 @@ int main()
         num[i] = (double)((double)rand() / (double)RAND_MAX);
     }
 
+    /** 
+     * Do stuff that use libm. Can be used both as a test to verify 
+     * that libm works but also as a benchmark.
+    */
     for(int i = 0; i < 1; i++)
     {
         for(int j = 0; j < 1; j++)
@@ -33,17 +38,22 @@ int main()
             res[9] = pow(M_PI, num[j]);
         }
     }
+
+    /** Print those 10 numbers */
     for(int i = 0; i < 10; i++) printf("%f ", num[i]);
 
+    /** Allocate a space from the heap */
     int alloc_size = 16384;
     void* buf = malloc(alloc_size);
     int element_size = sizeof(double);
 
+    /** Copy the doubles in there */
     for(int i = 0; i < alloc_size / element_size; i++) 
     {
         ((double*)buf)[i] = res[i % 10];
     }
 
+    /** Print the data saved in the buffer */
     for(int i = 0; i < alloc_size / element_size; i++) 
     {
         printf("%f ", ((double*)buf)[i]);
