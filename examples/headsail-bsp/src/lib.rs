211dd663ed8c545d7a067a647f3b649be621adf7
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2024-10-30 10:09:27 +0100

    refactor: organize lib.rs

--- examples/headsail-bsp/src/lib.rs
+++ examples/headsail-bsp/src/lib.rs
@@ -1,12 +1,31 @@
 //! A light-weight memory map based board support package for Headsail.
 #![no_std]
 
-pub mod apb_uart;
-#[cfg(feature = "sprint-apb-uart0")]
-pub mod sprintln;
+// Pick an optional PAC based on the target CPU. Some drivers may depend on it.
+#[cfg(feature = "hpc-pac")]
+pub use headsail_hpc_pac as pac;
+#[cfg(feature = "sysctrl-pac")]
+pub use headsail_sysctrl_pac as pac;
+
+// CPU specific modules
+#[cfg(feature = "hpc")]
+mod hpc;
 #[cfg(feature = "sysctrl")]
 pub mod sysctrl;
-pub mod sdram;
+#[cfg(feature = "hpc")]
+pub use hpc::*;
+
+#[cfg(feature = "alloc")]
+pub mod alloc;
+#[cfg(feature = "alloc")]
+pub use alloc::init_heap;
+
+// Timer implementation is somewhat different on hardware and VP. We pick and
+// re-export the correct one here.
+#[cfg(not(feature = "vp"))]
+mod apb_timer;
+#[cfg(feature = "vp")]
+mod timer_unit;
 pub mod timer {
     /*!
      * Timer module for Headsail. When running on the Renode
@@ -23,34 +42,22 @@ pub mod timer {
     #[cfg(feature = "vp")]
     pub use crate::timer_unit::*;
 }
+
+// Print-implementation specific modules
+#[cfg(feature = "sprint-apb-uart0")]
+pub mod sprintln;
+#[cfg(any(feature = "panic-apb-uart0", feature = "panic-sysctrl-uart"))]
+mod ufmt_panic;
+pub use ufmt;
+
+pub mod apb_uart;
+pub mod mmap;
+pub mod sdram;
 pub mod tb;
-#[cfg(feature = "hpc-pac")]
-pub use headsail_hpc_pac as pac;
-#[cfg(feature = "sysctrl-pac")]
-pub use headsail_sysctrl_pac as pac;
 
-#[cfg(not(feature = "vp"))]
-mod apb_timer;
-#[cfg(feature = "vp")]
-mod timer_unit;
-
-#[cfg(feature = "hpc")]
-pub use hpc::*;
 pub use riscv;
 #[cfg(feature = "rt")]
 pub use riscv_rt as rt;
-pub use ufmt;
-
-#[cfg(feature = "alloc")]
-pub mod alloc;
-#[cfg(feature = "hpc")]
-mod hpc;
-pub mod mmap;
-#[cfg(any(feature = "panic-apb-uart0", feature = "panic-sysctrl-uart"))]
-mod ufmt_panic;
-
-#[cfg(feature = "alloc")]
-pub use alloc::init_heap;
 
 /// # Safety
 ///
